0a758c1aa611b31300741714d4204be648ffbe3f
sgrunt <sgrunt1987@gmail.com>
2018-12-10 22:00:00 -0700

    v0.0.4

--- README.md
+++ README.md
@@ -4,19 +4,16 @@ Qine the Quadroguard, featuring both a PvP duel and a new party member.
 
 This is a CCLoader mod; if you don't already have CCLoader installed, grab it from here: https://github.com/CCDirectLink/CCLoader - then unpack this into new assets/mods/ directory in your CrossCode directory.
 
-There's currently no direct way to access the PvP arena; to get there, you can run the following from the console:
+Upon loading with this mod in place, Qine will appear in your party menu and can be invited / removed as normal. Contacting Qine will prompt you with the option to teleport to the PvP arena, effective upon returning to main gameplay.
 
+If you'd prefer to teleport directly there, you can issue the following from the console:
 ```javascript
 new frame.contentWindow.cc.ig.events.TELEPORT({"map": "mod/qine-arena"}).start()
 ```
 
 To add Qine to your party, run the following:
 ```javascript
-new cc.ig.events.SET_PARTY_MEMBER_SP_LEVEL({"member": "Qine", "level": "3"}).start()
-new cc.ig.events.SET_PARTY_MEMBER_LEVEL({"member": "Qine", "level": 60, "exp": 183, "updateEquipment": true }).start()
-new cc.ig.events.SET_PARTY_MEMBER_ALL_ELEMENTS({"member": "Qine", allElements: true }).start()
 new cc.ig.events.ADD_PARTY_MEMBER({"member": "Qine", "npc": {"global": true, "name": "Qine"}}).start()
 ```
-(Adjust the level and SP level as appropriate.)
 
 If you have questions, comments, or concerns, you can reach the author, sgrunt, on Discord in the CrossCode Discord server at http://discord.gg/crosscode.
--- qine/mod.js
+++ qine/mod.js
@@ -1,7 +1,16 @@
-if (!cc)
-    throw "Can't find CrossCode...";
+if (!cc) throw "Can't find CrossCode...";
 
-document.body.addEventListener('modsLoaded', () => {
+class Qine {
+  constructor() {
+    document.body.addEventListener("modsLoaded", () => this._init());
+  }
+
+  _init() {
+    this._addPartyMember();
+    this._addDatabaseEntries();
+  }
+
+  _addPartyMember() {
     /*
      * The idea here is to add a new entry to the list of party members,
      * and then call the functions that reference that list to reinit the
@@ -15,12 +24,78 @@ document.body.addEventListener('modsLoaded', () => {
     var d = ig.copy(sc.bu);
     d.unshift("Member3");
     d.unshift("Member2");
-    ig.Ja.W9a("party", function(a) {
-        return a.indexOf("Member") == 0 ? sc.sa.Kl(a.substr(6, 1) * 1 - 2) : sc.sa.LBb(a)
-    }, {
+    ig.Ja.W9a(
+      "party",
+      function(a) {
+        return a.indexOf("Member") == 0
+          ? sc.sa.Kl(a.substr(6, 1) * 1 - 2)
+          : sc.sa.LBb(a);
+      },
+      {
         b: "String",
         U: d
-    });
+      }
+    );
 
     sc.sa.f();
-});
+
+    const origLoad = cc.ig.gameMain[cc.ig.varNames.gameMainLoadMap];
+    cc.ig.gameMain[cc.ig.varNames.gameMainLoadMap] = data => {
+      const callOrig = origLoad.call(cc.ig.gameMain, data);
+      callOrig;
+
+      if (
+        cc.sc.party.Kn.Qine.status != undefined &&
+        cc.sc.party.Kn.Qine.status == 0
+      ) {
+        new cc.ig.events.SET_PARTY_MEMBER_SP_LEVEL({
+          member: "Qine",
+          level: "3"
+        }).start();
+        new cc.ig.events.SET_PARTY_MEMBER_LEVEL({
+          member: "Qine",
+          level: 60,
+          exp: 183,
+          updateEquipment: true
+        }).start();
+        new cc.ig.events.SET_PARTY_MEMBER_ALL_ELEMENTS({
+          member: "Qine",
+          allElements: true
+        }).start();
+        new cc.ig.events.SET_CONTACT_TYPE({
+          member: "Qine",
+          status: "FRIEND"
+        }).start();
+      }
+    };
+  }
+
+  _addDatabaseEntries() {
+    simplify.resources
+      .loadJSONPatched("data/qine-events.json")
+      .then(newDbEntries => {
+        cc.ig.Database.data.commonEvents["cooldown-S"].condition =
+          "party.alive.Emile || party.alive.Glasses || party.alive.Apollo || party.alive.Joern";
+        cc.ig.Database.data.commonEvents["cooldown-A"].condition =
+          "party.alive.Emile || party.alive.Glasses || party.alive.Apollo || party.alive.Joern";
+        cc.ig.Database.data.commonEvents["cooldown-B"].condition =
+          "party.alive.Emile || party.alive.Glasses || party.alive.Apollo || party.alive.Joern";
+
+        for (var key in newDbEntries) {
+          cc.ig.Database.data.commonEvents[key] = newDbEntries[key];
+        }
+
+        const nobody = cc.ig.Database.data.commonEvents["nobody-contact"];
+        delete cc.ig.Database.data.commonEvents["nobody-contact"];
+        cc.ig.Database.data.commonEvents["nobody-contact"] = nobody;
+
+        sc.Ip.Poa = {};
+        sc.Ip.f();
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+}
+
+new Qine();
