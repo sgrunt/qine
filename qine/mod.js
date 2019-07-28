d792860d61675f0f216dc0dfec7f8c8b820eed24
ac2pic <ac2pic@gmail.com>
2019-07-28 16:36:36 -0500

    Fixed but nobody came issue

--- qine/mod.js
+++ qine/mod.js
@@ -1,3 +1,27 @@
+sc.CommonEvents.inject({
+	_loadCommonEvents: function() {
+		// let it do it's thing
+		this.parent();
+		console.log('I successfully injected into it');
+		// going to find the indexOf no-contact
+		const socialActions = this.eventsByType["SOCIAL_ACTION"];
+		let i = 0;
+		for (i = 0; i < socialActions.length; i++) {
+			if (socialActions[i].name === "nobody-contact") {
+				break;
+			}
+		}
+		let noContactIndex = i;
+		// we have the index now find all the -contact and splice it
+		++i;
+		for (; i < socialActions.length;i++) {
+			if (socialActions[i].name.endsWith("-contact")) {
+				socialActions.splice(noContactIndex, 0, socialActions.splice(i, 1).pop());
+			}
+		}
+	}
+});
+
 class Qine {
   constructor() {
 	  this._init();
